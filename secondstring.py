242f836c309de73899e0b215fb486dcc15200f92
AzizDialloG <azizdiallo2000@outlook.com>
2020-02-14 19:49:40 +0000

    Added file secondstring that outputt every second letter in reverse order

---
+++ secondstring.py
@@ -0,0 +1,7 @@
+#Thierno Aziz Diallo
+#TASK3
+'''Write a program that takes asks a user to input a string and outputs every second letter in reverse order. '''
+
+firststr = str(input("Type a string: "))
+size = (len(firststr))
+print(firststr[-1:-size:-2])
