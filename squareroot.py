b2b1bcf114c68d818baec4d629125ab0171b4ae4
AzizDialloG <60354793+AzizDialloG@users.noreply.github.com>
2020-03-04 20:49:07 +0000

    Update squareroot.py
    Edit the program

--- squareroot.py
+++ squareroot.py
@@ -13,25 +13,31 @@ new_abs= abs- ((abs ** 2 -x)/ (2 * abs))
 tolerance= new_abs - abs
 """
 import math
-def square_root(x,y,tolerance =10000):
+def square_num(number):
+    result= math.sqrt(number)
+    return result
+def square_root(x,y,tolerance = 66666): #define functions
     abs = x
-    #tolerance = 10000
-    #new_abs=0.0001
+    #tolerance = 100
+    new_abs=0.001
     count= 0
+    
     while tolerance > y:
-        new_abs= abs- ((abs ** 2 -x)/ (2 * abs))
+        new_abs= abs- ((abs ** 2 -x)/ (2 * abs)) #Newton Method
         tolerance= new_abs - abs
         if tolerance < 0:
             tolerance *= -1
             abs = new_abs
             count = count + 1
-        print('Square root calculated in', count,"abs")
-        return abs
+    print('Square root calculated in', count,"abs")
+    return abs
 
 rootsquare = float (input("Please enter a positive number:"))
-approx = float (input("Please enter an approximation value (like 0.0001 as example:"))
-print("The square root of" ,rootsquare, "is approximately", square_root (rootsquare,approx))
-#float("{0:.2f}".format(x))
-#print("The square root of:" ,rootsquare, "is approximately", square_root(rootsquare,approx,))
+approx = float (input("Please enter an approximation value (like 0.001 as example:")) #newton formula by calculing square root (approx)
+print('The Square root of',rootsquare, 'is approximately %1.f'% round((square_root(rootsquare,approx)),1))#around the decimal places to 1
+#print("The square root of" ,rootsquare, "is approximately", square_root (rootsquare,approx))
+
+
+
 
     
