1ffe9afad85425c687eb0e9d760c8ee0d7735630
AzizDialloG <60354793+AzizDialloG@users.noreply.github.com>
2020-03-08 21:58:36 +0000

    Update bmi.py
    Added some comments.

--- bmi.py
+++ bmi.py
@@ -1,12 +1,17 @@
 #Thierno Aziz Diallo
 #Calculate your body max index
 '''
-Write a program that calculates somebody's Body Mass Index (BMI).This
+Write a program that calculates somebody's Body Mass Index (BMI).
+This program calculates the person body mass index. 
+The inputs are the person's weight in kilograms and height in centimeters.
+The formula is:
+BMI= (weight / (height ** 2)
+
 
 '''
-Height= float (input("Enter your Height in Centimeter:"))
-Weight=float(input("Enter your Weight in Kilogram:"))
+Height= float (input("Enter your Height in Centimeter:")) #enter the height in centimeter
+Weight=float(input("Enter your Weight in Kilogram:")) # enter the weight in kilogram
 
-BMI = Weight / ((Height / 100) ** 2)
+BMI = Weight / ((Height / 100) ** 2) # BMI formula
 
-print("Your Body Max Indix is",BMI)
+print("Your Body Max Indix is",BMI) #Result of the body max index(BMI)
