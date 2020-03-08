57c33bac8c5649e73c99a1fa3252b727c2ee307f
AzizDialloG <60354793+AzizDialloG@users.noreply.github.com>
2020-03-08 22:07:41 +0000

    Update secondstring.py
    Added comments

--- secondstring.py
+++ secondstring.py
@@ -1,7 +1,8 @@
 #Thierno Aziz Diallo
 #TASK3
 '''Write a program that takes asks a user to input a string and outputs every second letter in reverse order. '''
+#This program prints each second string character, it starts by the last one.
 
 firststr = str(input("Type a string: "))
-size = (len(firststr))
-print(firststr[-1:-size:-2])
+size = (len(firststr)) # counts the number of character on the string.
+print(firststr[-1:-size:-2]) #prints each second character by starting with the last one.
